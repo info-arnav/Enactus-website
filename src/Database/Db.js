f1f5a881bfbafef2c0f4fe4b2d4b9a7fdd538daa
amaan <amaan.ali.ug22@nsut.ac.in>
2024-12-26 05:08:39 +0530

    Database folder created and all the data files are added inside it

---
+++ src/Database/Db.js
@@ -0,0 +1,29 @@
+export const teamMembers = [
+    {
+      name: "Alice Smith",
+      image:
+        "https://plus.unsplash.com/premium_photo-1678197937465-bdbc4ed95815?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3",
+      facebook: "https://www.facebook.com/alice.smith",
+      instagram: "https://www.instagram.com/alice.smith",
+      twitter: "https://www.twitter.com/alice_smith",
+      designation : "president",
+    },
+    {
+      name: "Bob Johnson",
+      image:
+        "https://plus.unsplash.com/premium_photo-1664536392896-cd1743f9c02c?q=80&w=2787&auto=format&fit=crop&ixlib=rb-4.0.3",
+      facebook: "https://www.facebook.com/bob.johnson",
+      instagram: "https://www.instagram.com/bob.johnson",
+      twitter: "https://www.twitter.com/bob_johnson",
+      designation : "vice-president",
+    },
+    {
+      name: "Charlie Davis",
+      image:
+        "https://plus.unsplash.com/premium_photo-1689539137236-b68e436248de?q=80&w=2942&auto=format&fit=crop&ixlib=rb-4.0.3",
+      facebook: "https://www.facebook.com/charlie.davis",
+      instagram: "https://www.instagram.com/charlie.davis",
+      twitter: "https://www.twitter.com/charliedavis",
+      designation : "director",
+    },
+  ];
