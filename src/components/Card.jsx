4edee0991612b0fdc7fdde79350d461e4101381f
JaiBansal007 <jai.bansal.jai@gmail.com>
2025-01-26 17:04:38 +0530

    hover scale

--- src/components/Card.jsx
+++ src/components/Card.jsx
@@ -3,7 +3,7 @@ function Card({member , index}) {
     return (
         <div
           key={index}
-          className="h-auto w-auto sm:min-w-[40vw] lg:min-w-[23vw] max-w-[90vw] sm:max-w-[45vw] lg:max-w-[30vw] relative flex justify-center flex-col text-left rounded-2xl overflow-hidden transform transition  mx-auto  hover:shadow-[0px_8px_16px_rgba(189,183,107)]"
+          className="h-auto w-auto sm:min-w-[40vw] lg:min-w-[23vw] max-w-[90vw] sm:max-w-[45vw] lg:max-w-[30vw] relative flex justify-center flex-col text-left rounded-2xl overflow-hidden transform transition  mx-auto  hover:shadow-[0px_8px_16px_rgba(189,183,107)] hover:scale-105"
         >
           <img
             src={member.image}
